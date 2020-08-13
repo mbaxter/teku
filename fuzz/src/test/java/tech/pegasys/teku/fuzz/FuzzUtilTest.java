358b2d7bda57325e120c4cfa8fb23ec3bd73f925
Meredith Baxter <meredith.baxter@consensys.net>
2020-08-13 18:30:48 -0400

    Add tests for remaining FuzzUtil methods

--- fuzz/src/test/java/tech/pegasys/teku/fuzz/FuzzUtilTest.java
+++ fuzz/src/test/java/tech/pegasys/teku/fuzz/FuzzUtilTest.java
@@ -15,21 +15,35 @@ package tech.pegasys.teku.fuzz;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Optional;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.junit.BouncyCastleExtension;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import tech.pegasys.teku.core.BlockProcessorUtil;
-import tech.pegasys.teku.core.exceptions.BlockProcessingException;
+import tech.pegasys.teku.datastructures.blocks.BeaconBlock;
+import tech.pegasys.teku.datastructures.blocks.SignedBeaconBlock;
+import tech.pegasys.teku.datastructures.operations.Attestation;
+import tech.pegasys.teku.datastructures.operations.AttesterSlashing;
 import tech.pegasys.teku.datastructures.operations.Deposit;
-import tech.pegasys.teku.datastructures.operations.DepositWithIndex;
+import tech.pegasys.teku.datastructures.operations.ProposerSlashing;
+import tech.pegasys.teku.datastructures.operations.SignedVoluntaryExit;
 import tech.pegasys.teku.datastructures.state.BeaconState;
 import tech.pegasys.teku.datastructures.state.BeaconStateImpl;
 import tech.pegasys.teku.datastructures.util.DataStructureUtil;
 import tech.pegasys.teku.datastructures.util.SimpleOffsetSerializer;
+import tech.pegasys.teku.fuzz.input.AttestationFuzzInput;
+import tech.pegasys.teku.fuzz.input.AttesterSlashingFuzzInput;
+import tech.pegasys.teku.fuzz.input.BlockFuzzInput;
+import tech.pegasys.teku.fuzz.input.BlockHeaderFuzzInput;
 import tech.pegasys.teku.fuzz.input.DepositFuzzInput;
-import tech.pegasys.teku.ssz.SSZTypes.SSZList;
+import tech.pegasys.teku.fuzz.input.ProposerSlashingFuzzInput;
+import tech.pegasys.teku.fuzz.input.VoluntaryExitFuzzInput;
+import tech.pegasys.teku.util.config.Constants;
 
 @ExtendWith(BouncyCastleExtension.class)
 class FuzzUtilTest {
@@ -49,40 +63,152 @@ class FuzzUtilTest {
 
   // *************** START Deposit Tests *****************
 
-  @Test
-  void shouldProcessRandomDepositEquivalently() {
-    // TODO if the randomBeaconState might not be "valid", we want to use a fixed BeaconState that
-    // is correct
-    // TODO check: unlikely that this is a valid deposit for the beaconState?
-    BeaconState beaconState = dataStructureUtil.randomBeaconState();
-    SSZList<DepositWithIndex> deposits = dataStructureUtil.newDeposits(1);
-    Deposit deposit = deposits.get(0);
-    DepositFuzzInput input = new DepositFuzzInput(fromBeaconState(beaconState), deposit);
-    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
-    Optional<byte[]> actual = fuzzUtil.fuzzDeposit(rawInput);
-
-    try {
-      BeaconState postState =
-          beaconState.updated(
-              state -> {
-                BlockProcessorUtil.process_deposits(state, deposits);
-              });
-      byte[] expected = SimpleOffsetSerializer.serialize(postState).toArrayUnsafe();
-      assertThat(actual)
-          .hasValueSatisfying(
-              b -> {
-                assertThat(b).isEqualTo(expected);
-              });
-
-    } catch (BlockProcessingException e) {
-      // actual should be empty
-      assertThat(actual).isEmpty();
-    }
+  @AfterEach
+  public void cleanup() {
+    Constants.setConstants("minimal");
+    SimpleOffsetSerializer.setConstants();
   }
 
-  // *************** END Deposit Tests *******************
+  @Test
+  public void fuzzAttestation_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
 
-  // *************** START Shuffling Tests ***************
+    final Path testCaseDir = Path.of("minimal/operations/attestation/pyspec_tests/success");
+    final Attestation data = loadSsz(testCaseDir.resolve("attestation.ssz"), Attestation.class);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    AttestationFuzzInput input = new AttestationFuzzInput((BeaconStateImpl) preState, data);
+    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+    Optional<Bytes> result = fuzzUtil.fuzzAttestation(rawInput).map(Bytes::wrap);
+
+    Bytes expected = SimpleOffsetSerializer.serialize(postState);
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(expected);
+  }
+
+  @Test
+  public void fuzzAttesterSlashing_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
+
+    final Path testCaseDir =
+        Path.of("minimal/operations/attester_slashing/pyspec_tests/success_surround");
+    final AttesterSlashing data =
+        loadSsz(testCaseDir.resolve("attester_slashing.ssz"), AttesterSlashing.class);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    AttesterSlashingFuzzInput input =
+        new AttesterSlashingFuzzInput((BeaconStateImpl) preState, data);
+    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+    Optional<Bytes> result = fuzzUtil.fuzzAttesterSlashing(rawInput).map(Bytes::wrap);
+
+    Bytes expected = SimpleOffsetSerializer.serialize(postState);
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(expected);
+  }
+
+  @Test
+  public void fuzzBlock_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
+
+    final Path testCaseDir = Path.of("minimal/sanity/blocks/pyspec_tests/attestation");
+    final SignedBeaconBlock block0 =
+        loadSsz(testCaseDir.resolve("blocks_0.ssz"), SignedBeaconBlock.class);
+    final SignedBeaconBlock block1 =
+        loadSsz(testCaseDir.resolve("blocks_1.ssz"), SignedBeaconBlock.class);
+    final List<SignedBeaconBlock> blocks = List.of(block0, block1);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    BeaconState currentState = preState;
+    for (SignedBeaconBlock block : blocks) {
+      BlockFuzzInput input = new BlockFuzzInput((BeaconStateImpl) currentState, block);
+      byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+      Optional<Bytes> result = fuzzUtil.fuzzBlock(rawInput).map(Bytes::wrap);
+      assertThat(result).isNotEmpty();
+      currentState = SimpleOffsetSerializer.deserialize(result.get(), BeaconStateImpl.class);
+    }
+
+    assertThat(currentState).isNotNull();
+    assertThat(currentState).isEqualTo(postState);
+  }
+
+  @Test
+  public void fuzzBlockHeader_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
+
+    final Path testCaseDir =
+        Path.of("minimal/operations/block_header/pyspec_tests/success_block_header");
+    final BeaconBlock data = loadSsz(testCaseDir.resolve("block.ssz"), BeaconBlock.class);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    BlockHeaderFuzzInput input = new BlockHeaderFuzzInput((BeaconStateImpl) preState, data);
+    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+    Optional<Bytes> result = fuzzUtil.fuzzBlockHeader(rawInput).map(Bytes::wrap);
+
+    Bytes expected = SimpleOffsetSerializer.serialize(postState);
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(expected);
+  }
+
+  @Test
+  public void fuzzDeposit_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
+
+    final Path testCaseDir = Path.of("minimal/operations/deposit/pyspec_tests/success_top_up");
+    final Deposit data = loadSsz(testCaseDir.resolve("deposit.ssz"), Deposit.class);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    DepositFuzzInput input = new DepositFuzzInput((BeaconStateImpl) preState, data);
+    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+    Optional<Bytes> result = fuzzUtil.fuzzDeposit(rawInput).map(Bytes::wrap);
+
+    Bytes expected = SimpleOffsetSerializer.serialize(postState);
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(expected);
+  }
+
+  @Test
+  public void fuzzProposerSlashing_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
+
+    final Path testCaseDir = Path.of("minimal/operations/proposer_slashing/pyspec_tests/success");
+    final ProposerSlashing data =
+        loadSsz(testCaseDir.resolve("proposer_slashing.ssz"), ProposerSlashing.class);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    ProposerSlashingFuzzInput input =
+        new ProposerSlashingFuzzInput((BeaconStateImpl) preState, data);
+    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+    Optional<Bytes> result = fuzzUtil.fuzzProposerSlashing(rawInput).map(Bytes::wrap);
+
+    Bytes expected = SimpleOffsetSerializer.serialize(postState);
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(expected);
+  }
+
+  @Test
+  public void fuzzVoluntaryExit_minimal() {
+    final FuzzUtil fuzzUtil = new FuzzUtil(false, true);
+
+    final Path testCaseDir = Path.of("minimal/operations/voluntary_exit/pyspec_tests/success");
+    final SignedVoluntaryExit data =
+        loadSsz(testCaseDir.resolve("voluntary_exit.ssz"), SignedVoluntaryExit.class);
+    final BeaconState preState = loadSsz(testCaseDir.resolve("pre.ssz"), BeaconStateImpl.class);
+    final BeaconState postState = loadSsz(testCaseDir.resolve("post.ssz"), BeaconStateImpl.class);
+
+    VoluntaryExitFuzzInput input = new VoluntaryExitFuzzInput((BeaconStateImpl) preState, data);
+    byte[] rawInput = SimpleOffsetSerializer.serialize(input).toArrayUnsafe();
+    Optional<Bytes> result = fuzzUtil.fuzzVoluntaryExit(rawInput).map(Bytes::wrap);
+
+    Bytes expected = SimpleOffsetSerializer.serialize(postState);
+    assertThat(result).isNotEmpty();
+    assertThat(result.get()).isEqualTo(expected);
+  }
 
   @Test
   void shuffleInsufficientInput() {
@@ -112,83 +238,15 @@ class FuzzUtilTest {
             });
   }
 
-  // *************** END Shuffling Tests *****************
-
-  // *************** Helpers *****************************
-
-  // Because Fuzz input constructors want a BeaconStateImpl (for introspection)
-  // but the DataStructureUtil functions returns a BeaconState
-  private BeaconStateImpl fromBeaconState(BeaconState state) {
-    return new BeaconStateImpl(
-        state.getGenesis_time(),
-        state.getGenesis_validators_root(),
-        state.getSlot(),
-        state.getFork(),
-        state.getLatest_block_header(),
-        state.getBlock_roots(),
-        state.getState_roots(),
-        state.getHistorical_roots(),
-        state.getEth1_data(),
-        state.getEth1_data_votes(),
-        state.getEth1_deposit_index(),
-        state.getValidators(),
-        state.getBalances(),
-        state.getRandao_mixes(),
-        state.getSlashings(),
-        state.getPrevious_epoch_attestations(),
-        state.getCurrent_epoch_attestations(),
-        state.getJustification_bits(),
-        state.getPrevious_justified_checkpoint(),
-        state.getCurrent_justified_checkpoint(),
-        state.getFinalized_checkpoint());
+  public <T> T loadSsz(final Path path, final Class<T> type) {
+    try {
+      final byte[] data =
+          getClass().getClassLoader().getResourceAsStream(path.toString()).readAllBytes();
+      return SimpleOffsetSerializer.deserialize(Bytes.wrap(data), type);
+    } catch (final IOException e) {
+      throw new UncheckedIOException(e);
+    } catch (final NullPointerException e) {
+      throw new RuntimeException("Unable to locate file at " + path.toString(), e);
+    }
   }
-
-  /*// copied from BlockProcessorUtilTest
-  // TODO move from both into DataStructureUtil?
-  private BeaconState createBeaconState() {
-    return createBeaconState(false, null, null);
-  }
-
-  private BeaconState createBeaconState(UnsignedLong amount, Validator knownValidator) {
-    return createBeaconState(true, amount, knownValidator);
-  }
-
-  private BeaconState createBeaconState(
-      boolean addToList, UnsignedLong amount, Validator knownValidator) {
-    return BeaconState.createEmpty()
-        .updated(
-            beaconState -> {
-              beaconState.setSlot(dataStructureUtil.randomUnsignedLong());
-              beaconState.setFork(
-                  new Fork(
-                      Constants.GENESIS_FORK_VERSION,
-                      Constants.GENESIS_FORK_VERSION,
-                      UnsignedLong.valueOf(Constants.GENESIS_EPOCH)));
-
-              SSZMutableList<Validator> validatorList =
-                  SSZList.createMutable(
-                      Arrays.asList(
-                          dataStructureUtil.randomValidator(),
-                          dataStructureUtil.randomValidator(),
-                          dataStructureUtil.randomValidator()),
-                      Constants.VALIDATOR_REGISTRY_LIMIT,
-                      Validator.class);
-              SSZMutableList<UnsignedLong> balanceList =
-                  SSZList.createMutable(
-                      Arrays.asList(
-                          dataStructureUtil.randomUnsignedLong(),
-                          dataStructureUtil.randomUnsignedLong(),
-                          dataStructureUtil.randomUnsignedLong()),
-                      Constants.VALIDATOR_REGISTRY_LIMIT,
-                      UnsignedLong.class);
-
-              if (addToList) {
-                validatorList.add(knownValidator);
-                balanceList.add(amount);
-              }
-
-              beaconState.getValidators().addAll(validatorList);
-              beaconState.getBalances().addAll(balanceList);
-            });
-  }*/
 }
